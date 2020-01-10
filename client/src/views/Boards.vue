9a9e0e056b3a9caab5ad74cdc7868fc46137f8e4
Logan Garay <garay.logan@gmail.com>
2020-01-10 16:37:06 -0700

    styled boards view

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="background container-fluid">
     <div class="row">
-      <div class="col d-flex justify-content-between">
-        <h4>Here are your bans of Kan!!!</h4>
-        <div class="div d-flex">
-          <p>{{user.name}} ||</p>
+      <div class="col-8 offset-2 text-center">
+        <h4 class="title">Here are your bans of Kan!!!</h4>
+      </div>
+      <div class="col-2">
+        <div class="div d-flex justify-content-end">
+          <p class="username">{{user.name}}</p>
           <button @click="logout" class="btn btn-danger">Logout</button>
         </div>
       </div>
@@ -24,15 +26,22 @@
           <button type="submit">Create Board</button>
         </form>
       </div>
-      <div class="col-5 offset-3">
+      <div class="boardsList col-5 offset-3">
         <div>
-          <h4>Your boards</h4>
+          <h4 class="boardsTitle">Your boards</h4>
         </div>
-        <div v-for="board in boards" :key="board._id">
-          <router-link :to="{name: 'board', params: {boardId: board._id}}">{{board.title}}</router-link>
-          || {{board.description}}
-          <i class="fas fa-trash-alt" @click="deleteBoard(board._id)"></i>
-        </div>
+        <ul v-for="board in boards" :key="board._id">
+          <li class="listItem">
+            <router-link
+              id="routerLink"
+              :to="{name: 'board', params: {boardId: board._id}}"
+            >{{board.title}}</router-link>||
+            <span>{{board.description}}</span>
+            <span>
+              <i class="fas fa-trash-alt" @click="deleteBoard(board._id)"></i>
+            </span>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -83,5 +92,47 @@ export default {
   background-repeat: no-repeat;
   background-position: center;
   background-attachment: fixed;
+}
+.title {
+  font-size: 5rem;
+  font-family: "Jacques Francois Shadow", cursive;
+}
+.username {
+  font-size: 18pt;
+  margin-right: 10pt;
+  margin-bottom: 0;
+  display: flex;
+  align-items: center;
+}
+.listItem {
+  text-align: left;
+  padding-left: 20px;
+}
+ul {
+  list-style: none;
+}
+ul li {
+  color: white;
+}
+ul li span {
+  color: white;
+}
+li::before {
+  content: "🍓";
+  padding-right: 15px;
+}
+
+.boardsList {
+  background-color: rgb(255, 0, 0, 0.4);
+}
+.boardsList:hover {
+  background-color: #c20426;
+}
+#routerLink {
+  color: green;
+  text-shadow: 1px 1px 1px black;
+}
+.boardsTitle {
+  color: white;
 }
 </style>
